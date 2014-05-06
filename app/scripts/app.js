4f4ce2df8df1e83ccc2549d321da30107d74276f
jeffwesson <jeff@jeffwesson.com>
2014-05-06 03:00:50 -0700

    initial 2.0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular
-  .module('wdesignsApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('wdesignsApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
