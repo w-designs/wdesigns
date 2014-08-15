5a61b8bc3a043632e84d7b048b4358a5700439c7
jeffwesson <jeff@jeffwesson.com>
2014-08-14 20:19:49 -0700

    further fleshing out services

--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -8,6 +8,7 @@
  * Controller of the wdesignsApp
  */
 angular.module('wdesignsApp')
-  .controller('ServicesCtrl', function ($scope, wdesignsData) {
-    this.services = wdesignsData.content.services;
+  .controller('ServicesCtrl', function($scope, wdesignsData) {
+    this.webServices = wdesignsData.content.webServices;
+    this.graphicServices = wdesignsData.content.graphicServices;
   });
